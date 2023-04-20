2985075dd660f468cb91b0574b0fc1768084c4e9
L Carver <wifihum@gmail.com>
2023-04-20 16:37:49 -0700

    Add project files.

---
+++ source/std_for_each_example.cpp
@@ -0,0 +1,45 @@
+//------------------------------------------------------------------------------
+// std_for_each_example.cpp : 
+// 
+// Demos the std::for_each template function with std::vector
+// 
+// Author: https://cplusplus.com/reference/algorithm/for_each/
+// Modified by: Prof. Linda C
+//------------------------------------------------------------------------------
+
+#include <iostream>
+#include <vector>
+
+//------------------------------------------------------------------------------
+// using symbols
+//------------------------------------------------------------------------------
+using std::cout;
+using std::string;
+using std::vector;
+
+//------------------------------------------------------------------------------
+// called by for_each
+//------------------------------------------------------------------------------
+void myfunction(int i) {  // function:
+    std::cout << ' ' << i;
+}
+
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
+#include <iostream>     // std::cout
+#include <algorithm>    // std::for_each
+#include <vector>       // std::vector
+
+int main() {
+    vector<int> myvector;
+    myvector.push_back(10);
+    myvector.push_back(20);
+    myvector.push_back(30);
+
+    cout << "myvector contains:";
+    for_each(myvector.begin(), myvector.end(), myfunction);
+    std::cout << '\n';
+
+    return 0;
+}
